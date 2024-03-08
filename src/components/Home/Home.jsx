435b4082da88a81d13648c8245522e339611ce03
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-03-08 17:59:05 +0530

    Add authentication and navigation to Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useAuth } from '../../Context/useAuth'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
+    const auth= useAuth()
+    const handleClick = () => {
+        console.log('Home')
+        auth.login()
+    }
+    console.log(auth)
   return (
-    <div>Home</div>
+    <>
+    <h1>{auth.user}</h1>
+    <button onClick={()=>handleClick()}>Home</button>
+
+    <Link to="/contact">About</Link>
+    </>
   )
 }
 

1b06b4f753fe7533c8343de69d13bf63d572d4f6
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-03-08 17:58:55 +0530

    Add authentication and routing components

--- src/App.js
+++ src/App.js
@@ -5,31 +5,64 @@ import {
   Routes,
   Route,
   Link,
+  Navigate
 } from "react-router-dom";
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
+import { AuthProvider } from './Context';
+import { useAuth } from './Context/useAuth';
+import RequireAuth from './RequireAuth';
+import SignUp from './components/Signup/Signup';
+import Login from './components/Login/Login';
 function App() {
+
+  const auth = useAuth()
+  console.log(auth)
   return (
-    <Router>
+    <AuthProvider>
+      <Router>
         <Routes>
+          {/* <Switch> */}
+          <Route element={<RequireAuth />}>
             <Route
-                exact
-                path="/"
-                element={<Home />}
+              exact
+              path="/home"
+              element={<Home />}
             ></Route>
+          </Route>
+
+          <Route path="/" element={<Navigate replace to="/home" />} />
+
+          <Route
+            exact
+            path="/about"
+            element={<About />}
+          ></Route>
+          
+          <Route
+            exact
+            path="/login"
+            element={<Login />}
+          ></Route>
+          
+          <Route
+            exact
+            path="/signup"
+            element={<SignUp />}
+          ></Route>
+
+          <Route element={<RequireAuth />}>
             <Route
-                exact
-                path="/about"
-                element={<About />}
+              exact
+              path="/contact"
+              element={<Contact />}
             ></Route>
-            <Route
-                exact
-                path="/contact"
-                element={<Contact />}
-            ></Route>
+          </Route>
+          {/* </Switch> */}
         </Routes>
-</Router>
+      </Router>
+    </AuthProvider>
   );
 }
 

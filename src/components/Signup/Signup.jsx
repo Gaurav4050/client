6a3d08d6df247da72aa92113e3ee9f5e9202e8d5
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-03-09 10:57:10 +0530

    Refactor Signup component and add image upload functionality

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,35 +1,64 @@
 // Signup.js
 import axios from 'axios';
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { set, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const Signup = () => {
+    const navigate = useNavigate();
     const { register, handleSubmit, formState: { errors }, watch } = useForm();
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
     const [loading, setLoading] = useState(false);
-    const [profilePicture, setProfilePicture] = useState(null); 
+    const [profilePicture, setProfilePicture] = useState(null);
+    const [uploadaedImageURL, setUploadedImageURL] = useState(null);
     const password = watch("password");
 
     const onSubmit = async(data) => {
-        const payload = {
-            username:data.username,
-            email:data.email,
-            password:data.password,
-            // profilePicture:profilePicture
+       
+        try{
+            const payload = {
+                username:data.name,
+                email:data.email,
+                password:data.password,
+                profilePicUrl:uploadaedImageURL || ''
+            }
+            const response = await axios.post(`${process.env.REACT_APP_BASE_URI}/signup`, payload);
+            // navigate to login page
+            navigate('/login');
+            toast.success("account created successfully");
+        }catch(error){
+            toast.error("account already exists with this email");    
+            console.log(error);
         }
-        const response = await axios.post(`${process.env.REACT_APP_BASE_URI}/signup`, payload);
-        console.log(response);
+        
     };
 
-    const handleFileChange = (e) => {
+    const handleFileChange = async(e) => {
         const file = e.target.files[0];
+
+
         if (file) {
             const reader = new FileReader();
             reader.onloadend = () => {
                 setProfilePicture(reader.result);
             };
             reader.readAsDataURL(file);
+        }
+
+        if(file){
+            try{
+
+                const formdata = new FormData();
+                formdata.append('uploadFile', file);
+
+                const response= await axios.post(`${process.env.REACT_APP_BASE_URI}/upload`, formdata);
+                setUploadedImageURL(response.data.result.url);
+            }catch(error){
+                toast.error("failed to upload image");
+                console.log(error);
+            }
         }
     };
     return (
@@ -62,8 +91,20 @@ const Signup = () => {
                         <label htmlFor="password" className="block mb-1">Password</label>
                         <input type="password"
                         placeholder='Enter your password'
-                        id="password" name="password" className="w-full border rounded px-3 py-2" {...register("password",{ required: true })} />
-                        {errors.password && <p className="text-red-500 text-xs mt-1">Please enter password</p>}
+                        id="password" name="password" className="w-full border rounded px-3 py-2" 
+                        {...register("password",{ required: {
+                            value: true, 
+                            message: "Password is required"
+                        },
+                            pattern: {
+                                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/,
+                                message: "Please enter a password with at least 8 characters, including at least one uppercase letter, one lowercase letter, one number, and one special character (!@#$%^&*)."
+                            }
+                        
+                         })}
+                        
+                        
+                        />
                     </div>
                     {/* confirm password field */}
                     <div className="mb-4">
@@ -71,9 +112,15 @@ const Signup = () => {
                         <input type="password"
                         placeholder='Confirm password'
                         id="confirmPassword" name="confirmPassword" className="w-full border rounded px-3 py-2" {...register("confirmPassword",{ required: true, validate: (value) => value === password || "Passwords do not match" })} />
-                        {errors.confirmPassword && <p className="text-red-500 text-xs mt-1">{errors.confirmPassword.message}</p>}
+                    {errors.confirmPassword && <p className="text-red-500 text-xs mt-1">{errors.confirmPassword.message}</p>}
+
                     </div>
+
+
                     </div>
+
+                    {errors.password && <p className="text-red-500 text-xs mt-[-10px] mb-3">{errors.password.message}</p>}
+
 
                      {/* Profile Picture field */}
                      <div className="mb-4">

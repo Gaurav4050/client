a109dfb8623944802d123e49c3288ba07b699799
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-03-09 10:56:35 +0530

    Remove unused components and add toast notification

--- src/App.js
+++ src/App.js
@@ -8,19 +8,17 @@ import {
   Navigate
 } from "react-router-dom";
 import Home from './components/Home/Home';
-import About from './components/About/About';
-import Contact from './components/Contact/Contact';
 import { AuthProvider } from './Context';
-import { useAuth } from './Context/useAuth';
 import RequireAuth from './RequireAuth';
 import SignUp from './components/Signup/Signup';
 import Login from './components/Login/Login';
+import toast, { Toaster } from 'react-hot-toast';
 function App() {
 
-  const auth = useAuth()
-  console.log(auth)
+
   return (
     <AuthProvider>
+      <Toaster />
       <Router>
         <Routes>
           {/* <Switch> */}
@@ -36,12 +34,6 @@ function App() {
 
           <Route
             exact
-            path="/about"
-            element={<About />}
-          ></Route>
-          
-          <Route
-            exact
             path="/login"
             element={<Login />}
           ></Route>
@@ -52,13 +44,7 @@ function App() {
             element={<SignUp />}
           ></Route>
 
-          <Route element={<RequireAuth />}>
-            <Route
-              exact
-              path="/contact"
-              element={<Contact />}
-            ></Route>
-          </Route>
+          
           {/* </Switch> */}
         </Routes>
       </Router>

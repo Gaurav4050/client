2c03529ed50aba90305a0482de7aa5afbd2b9496
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-03-09 10:57:03 +0530

    Refactor AuthProvider component to handle user login and logout

--- src/Context/index.js
+++ src/Context/index.js
@@ -4,43 +4,57 @@ const defaultProvider = {
     user: "",
     setUser: () => {},
     login: () => {},
+    logout: () => {},
 };
 
 const AuthContext = createContext(defaultProvider);
 
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(defaultProvider.user);
+    const [isInitialized, setIsInitialized] = useState(false);
 
     useEffect(() => {
-        const userData = localStorage.getItem('userData')
+        const initAuth = () => {
+            const userData = localStorage.getItem('userData')
+        console.log(userData)
         if (userData) {
-          setUser(JSON.parse(userData))
+            const paramseData = JSON.parse(userData)
+            console.log(paramseData)
+          setUser(paramseData)
         }
+        setIsInitialized(true)
+        }
+
+        
+    initAuth()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
       }, [])
 
-    const handleLogin = (params, errorCallback) => {
-        // LoginApi(params)
-        //   .then(async res => {
-        //     setUser(res.data)
-        //     localStorage.setItem('userData', JSON.stringify(res.data))
-        //   })
-        //   .catch(err => {
-        //     console.log(err)
-        //   })
-        setUser("rajast")
+
+      console.log("user", user)
+    const handleLogin = (data) => {
+        console.log(data)
+        localStorage.setItem('userData', JSON.stringify(data))
+        setUser(data)
         
       }
 
+      const handleLogout = () => {
+        localStorage.removeItem('userData')
+        setUser(null)
+      }
+
       const value = {
-        user,
+        user: user,
         setUser,
         login: handleLogin,
+        logout: handleLogout,
       };
     
 
     return (
         <AuthContext.Provider value={value}>
-            {children}
+             {isInitialized && children}
         </AuthContext.Provider>
     );
 };

a09077c4b0afd0d06c256ef7b954fbe51bbc5136
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-03-09 10:57:19 +0530

    Add login functionality and display test credentials

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,52 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../Context/useAuth';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const Login = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+     const navigate = useNavigate();
+    const auth= useAuth()
 
-    const onSubmit = (data) => {
-        // Handle form submission, e.g., login request
-        console.log(data);
-        setShowSuccessMessage(true);
+    const onSubmit = async (data) => {
+        
+        try{
+
+            const payload = {
+                email:data.email,
+                password:data.password
+            }
+
+            const response = await axios.post(`${process.env.REACT_APP_BASE_URI}/login`, 
+            payload,
+            {
+                withCredentials: true
+              }
+            );
+            auth.login(response?.data?.data)
+            navigate('/home');
+            toast.success("login successful");
+
+        }catch(error){
+            toast.error("invalid credentials");
+            console.log(error);
+        }
     };
 
     return (
         <div className="min-h-screen flex justify-center items-center bg-gray-200">
             <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
                 <h2 className="text-2xl mb-4">Login</h2>
+                <div class="bg-blue-100 rounded p-4 mb-4">
+    <p class="text-sm text-blue-700 font-semibold">Test credentials:</p>
+    <div class="flex flex-col">
+        <span class="text-gray-600">email: test@gmail.com</span>
+        <span class="text-gray-600">password: Test@123</span>
+    </div>
+</div>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     {/* Email field */}
                     <div className="mb-4">
